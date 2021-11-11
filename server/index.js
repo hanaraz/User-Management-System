60283d2008a51262d79b1a0faa3d13513008354e
hanaraz <hana.raz07@gmail.com>
2021-11-11 16:07:38 +0200

    create database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
@@ -10,4 +11,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, () => console.log("server runs on port 3000"));
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("connected succesfully to the database"), err => console.log(err))
+
+app.listen(process.env.PORT, () => console.log("server runs on port 5000"));
